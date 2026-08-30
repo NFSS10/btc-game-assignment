38bf0c90fbcd576012b6054360c2cb5e5002fc1a
Nuno Silva <nuno_nfss96@hotmail.com>
2026-08-30 17:43:22 +0100

    feat: mantine charts setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { MantineProvider } from "@mantine/core";
 import { Game } from "~/components/widgets";
 
 import "@mantine/core/styles.css";
+import "@mantine/charts/styles.css";
 
 function App() {
     return (
